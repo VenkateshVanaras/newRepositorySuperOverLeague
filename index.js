4e46926975577870014e461b5c6238fa75c2449e
Venkatesh Vanaras <venkatesvanaras7@gmail.com>
2024-05-19 12:43:17 +0000

    super over league match details comminting.

---
+++ index.js
@@ -0,0 +1,19 @@
+const element = (
+  <div className="greeting">
+    <div>
+      <h1 className="hi"> Super Over League</h1>
+      <div>
+        <img
+          src="https://assets.ccbp.in/frontend/react-js/rcb-img.png"
+          className="imageClass"
+        />
+        <img
+          className="imageClass"
+          src="https://assets.ccbp.in/frontend/react-js/csk-img.png"
+        />
+      </div>
+    </div>
+  </div>
+);
+
+ReactDOM.render(element, document.getElementById("root"));
